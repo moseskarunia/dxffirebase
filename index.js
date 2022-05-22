1fdd96a4204c5ac43fe040f20d7edd51f41ae009
Moses Karunia <moseskarunia@gmail.com>
2022-05-22 13:56:32 +0700

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const { DxfParser, toGeojson } = require('dxf2json')
+
+const fileName = 'mine';
+
+new DxfParser().init(`./assets/${fileName}.dxf`)
+  .then(result => {
+    fs.writeFile(__dirname + `/results/${fileName}.json`, JSON.stringify(result), err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    })
+    console.log(result);
+    return result
+  })
+
+// const content = fs.readFileSync('./assets/r18.dxf', 'utf-8')
+// new DxfParser().parseContent(content)
+//   .then(result => {
+//     console.log(result);
+//     return toGeojson(result)
+//   })
