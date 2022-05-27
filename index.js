c6e13b570ba1de1c49d37e0468973c0578c925a5
Moses Karunia <moseskarunia@gmail.com>
2022-05-27 15:57:49 +0700

    check point

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 const fs = require('fs')
 const { DxfParser, toGeojson } = require('dxf2json')
 
-const fileName = '2a';
+// json
+// const fileName = '2a';
 
-new DxfParser().init(`./assets/${fileName}.dxf`)
-  .then(result => {
-    fs.writeFile(__dirname + `/results/${fileName}.json`, JSON.stringify(result), err => {
-      if (err) {
-        console.error(err)
-        return
-      }
-    })
-    console.log(result);
-    return result
-  })
-
-// const content = fs.readFileSync('./assets/r18.dxf', 'utf-8')
-// new DxfParser().parseContent(content)
+// new DxfParser().init(`./assets/${fileName}.dxf`)
 //   .then(result => {
+//     fs.writeFile(__dirname + `/results/${fileName}.json`, JSON.stringify(result), err => {
+//       if (err) {
+//         console.error(err)
+//         return
+//       }
+//     })
 //     console.log(result);
-//     return toGeojson(result)
+//     return result
 //   })
+
+// length
+const jsonFile = JSON.parse(fs.readFileSync('./results/2a.json', 'utf-8'));
+
+const entities = jsonFile.ENTITIES;
+
+let totalLength = 0;
+
+entities.forEach(v => {
+  if (v.type == 'ARC') {
+    let theta = v.endAngle - v.startAngle;
+
+    if (theta < 0) {
+      theta = 360 + theta;
+    }
+
+    const arcLength = 2 * Math.PI * v.radius * (theta / 360);
+    console.log(`Arc: ${arcLength}`);
+    totalLength += arcLength;
+  } else if (v.type == 'LINE') {
+    const p1 = v.vertices[0];
+    const p2 = v.vertices[1];
+    const distance = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+
+    console.log(`Line: ${distance}`);
+
+    totalLength += distance;
+  }
+});
+
+console.log(`Total: ${totalLength}`);
