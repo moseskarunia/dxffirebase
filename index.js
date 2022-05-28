d6880a1c9fd65577aecc8a3a2fb33813a466e0ab
Moses Karunia <moseskarunia@gmail.com>
2022-05-28 14:12:06 +0700

    solve

--- index.js
+++ index.js
@@ -17,13 +17,15 @@ const { DxfParser, toGeojson } = require('dxf2json')
 //   })
 
 // length
-const jsonFile = JSON.parse(fs.readFileSync('./results/2a.json', 'utf-8'));
+const jsonFile = JSON.parse(fs.readFileSync('./results/2.json', 'utf-8'));
 
 const entities = jsonFile.ENTITIES;
 
-let totalLength = 0;
+let totalCut = 0;
+let totalBend = 0;
 
 entities.forEach(v => {
+  const operation = v.lineType.toLowerCase() == 'continuous' ? 'cut' : 'bend';
   if (v.type == 'ARC') {
     let theta = v.endAngle - v.startAngle;
 
@@ -32,17 +34,38 @@ entities.forEach(v => {
     }
 
     const arcLength = 2 * Math.PI * v.radius * (theta / 360);
-    console.log(`Arc: ${arcLength}`);
-    totalLength += arcLength;
+    console.log(`Arc: ${arcLength} (${operation})`);
+
+    if (operation == 'cut') {
+      totalCut += arcLength;
+    } else if (operation == 'bend') {
+      totalBend += arcLength;
+    }
   } else if (v.type == 'LINE') {
     const p1 = v.vertices[0];
     const p2 = v.vertices[1];
     const distance = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
 
-    console.log(`Line: ${distance}`);
+    console.log(`Line: ${distance} (${operation})`);
+    if (operation == 'cut') {
+      totalCut += distance;
+    } else if (operation == 'bend') {
+      totalBend += distance;
+    }
+  } else if (v.type == 'CIRCLE') {
+    const circum = 2 * Math.PI * v.radius;
 
-    totalLength += distance;
+    console.log(`Circle: ${circum} (${operation})`);
+
+    if (operation == 'cut') {
+      totalCut += circum;
+    } else if (operation == 'bend') {
+      totalBend += circum;
+    }
+  } else {
+    console.log(`=========Unhandled type: ${v.type} (${operation})=========`)
   }
 });
 
-console.log(`Total: ${totalLength}`);
+console.log(`Total cut: ${totalCut}`);
+console.log(`Total bend: ${totalBend}`);
